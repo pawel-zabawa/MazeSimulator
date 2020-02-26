2619e7aa17d19f8ec8c1f6c30f593df422af86bd
pawel-zabawa <pawel.zabawa.108wds@gmail.com>
2020-02-26 22:08:58 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+    static public void main(String[] args)
+    {
+    int x = 4;
+    int y = 4;
+    int[][] mazeTable = new int[x][y];
+    mazeTable[0][0] = 1;
+    int[][] pointCoordinates = new int[1][1];
+
+    }
+}
