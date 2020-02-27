1b60b682e9f96371f9bc1930454bdf8de5823466
pawel-zabawa <pawel.zabawa.108wds@gmail.com>
2020-02-27 22:56:26 +0100

    Added Frame class, added KeyEvent for move player

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,40 @@
 import java.awt.event.KeyEvent;
 
 public class Main {
+
     static int playerX = 0;
     static int playerY = 0;
     int[][] player = new int[playerX][playerY];
-
-    public void keyPressed(KeyEvent evt) {
+    static Maze maze1 = new Maze(4,4);
+    public static void keyPressed(KeyEvent evt) {
     int key = evt.getKeyCode();
+    if (key == KeyEvent.VK_LEFT)
+    {
+    playerX--;
+    maze1.getMap();
+    }
+    if (key == KeyEvent.VK_RIGHT)
+    {playerX++;
+    maze1.getMap();
+    }
+    if (key == KeyEvent.VK_UP)
+    {playerY--;
+    maze1.getMap();
+    }
+    if (key == KeyEvent.VK_DOWN)
+    {playerY++;
+    maze1.getMap();
+    }
 
 
-}
+    }
 
-    static public void main(String[] args) {
-    Maze maze1 = new Maze(4,4);
+    static public void main(String[] args) throws InterruptedException {
+
     maze1.mazeMap[playerX][playerY] = 3;
     maze1.getMap();
+        Thread.currentThread().join();
+
 
 
 
