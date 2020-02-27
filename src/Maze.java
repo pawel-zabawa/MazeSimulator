7384da7a2c5c44cae911b92b46f50d94912c26d6
pawel-zabawa <pawel.zabawa.108wds@gmail.com>
2020-02-27 19:12:17 +0100

    Separate Maze class - creating Map Added Function to show map

---
+++ src/Maze.java
@@ -0,0 +1,40 @@
+public class Maze {
+    int x;
+    int y;
+    int[][] mazeMap;
+
+    Maze(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.mazeMap = new int[x][y];
+        mazeMap[0][0] = 1;
+        mazeMap[0][1] = 0;
+        mazeMap[0][2] = 0;
+        mazeMap[0][3] = 0;
+        mazeMap[1][0] = 1;
+        mazeMap[1][1] = 0;
+        mazeMap[1][2] = 0;
+        mazeMap[1][3] = 0;
+        mazeMap[2][0] = 1;
+        mazeMap[2][1] = 0;
+        mazeMap[2][2] = 0;
+        mazeMap[2][3] = 0;
+        mazeMap[3][0] = 1;
+        mazeMap[3][1] = 1;
+        mazeMap[3][2] = 1;
+        mazeMap[3][3] = 1;
+
+
+    }
+
+
+    void getMap() {
+        for (int i = 0; i < x; i++) {
+            System.out.println();
+            for (int j = 0; j < y; j++) {
+                System.out.print("[" + mazeMap[i][j] + "]");
+            }
+        }
+
+    }
+}
